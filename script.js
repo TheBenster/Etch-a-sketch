873d7f5ac0360162ce78cec662b7c66963402b9a
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-03-27 16:21:22 -0400

    create boilerplate site ready for work

---
+++ script.js
@@ -0,0 +1,12 @@
+let container = document.getElementById('#container')
+container.textContent = 'test'
+
+let draw = document.createElement('div')
+
+draw.style = 'background-color: red; height:20px; width:20px;'
+
+
+
+container.appendChild(draw)
+container.appendChild(draw)
+container.appendChild(draw)
