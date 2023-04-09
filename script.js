62aab4aab553a3ac8bfd3f7aa33ce7ec03d1a5f7
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-04-09 10:46:03 -0400

    finish etch-a-sketch, add logic to randomize, and make the clear button work

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let container = document.querySelector('#grid');
 let default_color = '#212121';
+let changedColor
+gridItem = document.getElementsByClassName('.grid-item')
+rawColor = document.querySelector('#colorpicker')
+
+rawColor.addEventListener('click', () => {
+  default_color = rawColor.value;
+})
 
 
 
@@ -9,10 +16,10 @@ function makeRows(rows, cols) {
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     cell.style = 'height:7px; width:7px; gap:0'
-    cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
+    cell.addEventListener('mouseover', () => {
+      cell.style.backgroundColor = default_color
+    })
   };
-};
-let gridItem = document.querySelector('.grid-item')
-
+}
 makeRows(16, 16);
