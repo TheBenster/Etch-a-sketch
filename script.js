a5c09b086a95743368359473e3c21d19bacf8bf5
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-04-09 13:02:40 -0400

    add finishing style changes and animations, ready to deploy to pages.

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ let default_color = '#212121';
 let changedColor
 gridItem = document.getElementsByClassName('.grid-item')
 rawColor = document.querySelector('#colorpicker')
+random = document.querySelector('#random')
+clear = document.querySelector('#clear')
+
 
 rawColor.addEventListener('click', () => {
   default_color = rawColor.value;
 })
+
+
 
 
 
@@ -19,6 +24,12 @@ function makeRows(rows, cols) {
     container.appendChild(cell).className = "grid-item";
     cell.addEventListener('mouseover', () => {
       cell.style.backgroundColor = default_color
+    })
+    clear.addEventListener('click', () => {
+      cell.style.backgroundColor = ''
+    })
+    random.addEventListener('click', () => {
+      default_color  = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0")
     })
   };
 }
